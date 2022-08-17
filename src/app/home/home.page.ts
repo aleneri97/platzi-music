d8547943dd779015bd301d6c4f5b9a31ddb68fb3
Manuel Neri <manuel.neri@pgr.gob.mx>
2022-08-17 17:47:23 -0500

    Cambia de componente el contenido del swiper (intro)
    El swiper se ubicará en un componente Intro, en la ruta /intro

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,32 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  slideOps = {
-    initialSlide: 0,
-    slidesPerView: 1,
-    centeredSlides: true,
-    speed: 400,
-  };
-  slides = [
-    {
-      title: 'Escucha tu música',
-      subtitle: 'En cualquier lugar',
-      description: 'Ullamco labore sunt deserunt minim Ullamco labore sunt deserunt minim.',
-      icon: 'caret-forward-outline',
-    },
-    {
-      title: 'Disfruta de nuestro reproductor',
-      subtitle: 'De videos increíbles',
-      description: 'Ullamco labore sunt deserunt minim Ullamco labore sunt deserunt minim.',
-      icon: 'videocam-sharp'
-    },
-    {
-      title: 'Accede al exclusivo',
-      subtitle: 'Modo deporte',
-      description: 'Ullamco labore sunt deserunt minim Ullamco labore sunt deserunt minim.',
-      icon: 'bicycle-sharp'
-    },
-  ];
 
   constructor() {}
 
---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+  slideOps = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    centeredSlides: true,
+    speed: 400,
+  };
+  slides = [
+    {
+      title: 'Escucha tu música',
+      subtitle: 'En cualquier lugar',
+      description: 'Ullamco labore sunt deserunt minim Ullamco labore sunt deserunt minim.',
+      icon: 'caret-forward-outline',
+    },
+    {
+      title: 'Disfruta de nuestro reproductor',
+      subtitle: 'De videos increíbles',
+      description: 'Ullamco labore sunt deserunt minim Ullamco labore sunt deserunt minim.',
+      icon: 'videocam-sharp'
+    },
+    {
+      title: 'Accede al exclusivo',
+      subtitle: 'Modo deporte',
+      description: 'Ullamco labore sunt deserunt minim Ullamco labore sunt deserunt minim.',
+      icon: 'bicycle-sharp'
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
