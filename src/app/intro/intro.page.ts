1c39e730326621a7d4b8d84edc030bea1174d4ec
Manuel Neri <manuel.neri@pgr.gob.mx>
2022-08-17 18:27:29 -0500

    Agrega e inicializa dependencia Storage-Angular

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-intro',
@@ -34,12 +35,14 @@ export class IntroPage implements OnInit {
     },
   ];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private storage: Storage) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.storage.create();
   }
 
   finish() {
+    this.storage.set('isIntroShowed', true);
     this.router.navigateByUrl('/home');
   }
 
