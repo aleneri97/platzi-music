4624c50473f6932aef5bd57a39e1cff394ba7f17
Alejandro Neri <ale_neri@yahoo.com>
2022-08-18 08:20:43 -0500

    Crea formulario de Login con validaciones y error hints

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+  validationMessages = {
+    email: [
+      { type: 'required', message:  'El email es requerido' },
+      { type: 'email', message:  'El email no es válido' },
+    ],
+    password: [
+      { type: 'required', message:  'La contraseña es requerida' },
+      { type: 'minlength', message:  'La contraseña debe de contener al menos 5 caracteres' },
+    ],
+  };
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
 
   ngOnInit() {
   }
