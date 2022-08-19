aabaf4c4b632f496fd9ac39effcf5c18ab6dd8c1
Manuel Neri <manuel.neri@pgr.gob.mx>
2022-08-19 18:46:20 -0500

    Corrige error de ruta default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { LoginGuard } from './guards/login.guard';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'menu',
+    redirectTo: 'menu/home',
     pathMatch: 'full',
   },
   {
@@ -25,7 +25,8 @@ const routes: Routes = [
     path: 'menu',
     loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule),
     canActivate: [IntroGuard, LoginGuard],
-  },  {
+  },
+  {
     path: 'songs-modal',
     loadChildren: () => import('./songs-modal/songs-modal.module').then( m => m.SongsModalPageModule)
   },
